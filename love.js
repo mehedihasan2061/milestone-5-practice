253d1841ddb5497d8e133cb3d144907d977179e8
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2022-08-15 16:10:42 +0600

    added

---
+++ love.js
@@ -0,0 +1,11 @@
+document.getElementById('yes-btn').addEventListener('click', function () {
+    const text = document.getElementById('text');
+    text.innerText = 'I Love You';
+    
+})
+document.getElementById('no-btn').addEventListener('click', function () {
+    const text = document.getElementById('text');
+    text.innerText = 'I Hate You!!';
+    
+})
+
